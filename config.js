8d5cb7033dbdb59b23a17418b122250ff4beb09d
ranjitgorai <gorai.ranjit13@gmail.com>
2017-02-03 17:03:40 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ module.exports = {
     "secret": "bd17ddffc6694d25834a99ac2708a498",
     //"database": "mongodb://localhost:27017/FormDatabase",
    "database":"mongodb://ranjitgorai:ranjit@ds139989.mlab.com:39989/ranjit",
-    "siteUrl": "http://localhost:3000"
+    "siteUrl": "http://localhost:3000",
     /*"email": {
       "from": "s26c.sayan@gmail.com",
       "host": "smtp-pulse.com",
@@ -19,7 +19,7 @@ module.exports = {
     "secret": "bd17ddffc6694d25834a99ac2708a498",
     //"database": "mongodb://localhost:27017/FormDatabase",
    "database":"mongodb://ranjitgorai:ranjit@ds139989.mlab.com:39989/ranjit",
-    "siteUrl": "http://localhost:3000"
+    "siteUrl": "http://localhost:3000",
    /* "email": {
       "from": "s26c.sayan@gmail.com",
       "host": "smtp-pulse.com",
